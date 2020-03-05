c3cc29fcc525610155d952f2079ffd91c10e3e65
Daniel Kersten <dkersten1994@gmail.com>
2020-03-05 22:30:37 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -16,7 +16,7 @@ def hopper
         }
      }
     return programmer_hash[:grace_hopper]
-    return programmer_hash[:alan_kay][:alan_kay_is_known_for]
+    return programmer_hash[:alan_kay][:known_for]
 
 end
 
